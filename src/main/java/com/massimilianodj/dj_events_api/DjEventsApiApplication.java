3914ad148eaed53adc83440e7ec1df4f05242816
utzeri-massimiliano <utzerimassimiliano.music@gmail.com>
2026-07-19 15:09:13 +0200

    Project structure and package organization

---
+++ src/main/java/com/massimilianodj/dj_events_api/DjEventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.massimilianodj.dj_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjEventsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/massimilianodj/dj_events_api/DjEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massimilianodj.dj_events_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjEventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
